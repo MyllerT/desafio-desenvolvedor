1f525e9a55de8aa4695f2e67f3ebcee43c33ba21
MyllerT <millertavares@yahoo.com.br>
2024-12-08 10:42:56 -0300

    fix: descrição da alteração necessária

--- Desafio/database/migrations/2024_12_06_205422_create_uploads_table.php
+++ Desafio/database/migrations/2024_12_06_205422_create_uploads_table.php
@@ -1,27 +1,26 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUploadsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('uploads', function (Blueprint $table) {
             $table->id();
+            $table->string('file_name')->unique();
+            $table->string('file_path');
+            $table->string('hash')->unique(); 
+            $table->timestamp('upload_date');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('uploads');
     }
-};
+}
